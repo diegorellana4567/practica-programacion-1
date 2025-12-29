4ec3c4c072f800187b78a37eedb569add6ac590a
diegorellana4567 <diegorellana4567@gmail.com>
2025-12-28 18:02:16 -0600

    Create validacion_password.py
    Primer ejercicio

---
+++ validacion_password.py
@@ -0,0 +1,15 @@
+# Mi primer programa: Validar contraseña
+# Autor: Diego Orellana
+contrasena_secreta = "Gatito123"
+intento_usuario = ""
+
+print("--- SISTEMA DE SEGURIDAD ---")
+
+# Mientras la contraseña sea incorrecta, seguimos preguntando
+while intento_usuario != contrasena_secreta:
+    intento_usuario = input("Ingrese la contraseña: ")
+    
+    if intento_usuario != contrasena_secreta:
+        print("Acceso denegado. Intente de nuevo.")
+
+print("¡Acceso Concedido! Bienvenido al sistema.")
